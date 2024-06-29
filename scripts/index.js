bb5a6caae7dd0a7f1aad5391024ac5b5cb104aaf
arjunsaseendran91 <arjunsaseendran@icloud.com>
2024-06-29 15:48:52 +0530

    header link selection highlighter

--- scripts/index.js
+++ scripts/index.js
@@ -8,22 +8,41 @@ const boxValue = document.getElementById("box-row");
 const headingValue = document.getElementById("heading");
 const oldParaValue = paraValue.innerHTML;
 const oldBoxValue = boxValue.innerHTML;
-const oldHeadingValue = headingValue.innerHTML
+const oldHeadingValue = headingValue.innerHTML;
+
+
+
+
 
 
 handleHome.addEventListener("click", () => {
+    handleHome.style.color = '#ACD793'
+    handleAbout.style.color = "#B2C8BA";
+    handlePortfolio.style.color = "#B2C8BA";
+    handleServices.style.color = "#B2C8BA";
+    handleContact.style.color = "#B2C8BA";
   headingValue.innerHTML = oldHeadingValue;
   boxValue.innerHTML = oldBoxValue;
   paraValue.innerHTML = oldParaValue;
 });
 
 handleAbout.addEventListener("click", () => {
+    handleHome.style.color = '#B2C8BA'
+    handleAbout.style.color = "#ACD793";
+    handlePortfolio.style.color = "#B2C8BA";
+    handleServices.style.color = "#B2C8BA";
+    handleContact.style.color = "#B2C8BA";
   headingValue.innerHTML = "About Me";
   boxValue.innerHTML = "";
   paraValue.innerHTML = paraAbout;
 });
 
 handlePortfolio.addEventListener("click", () => {
+    handleHome.style.color = '#B2C8BA'
+    handleAbout.style.color = "#B2C8BA";
+    handlePortfolio.style.color = "#ACD793";
+    handleServices.style.color = "#B2C8BA";
+    handleContact.style.color = "#B2C8BA";
   headingValue.innerHTML= "Portfolio";
   boxValue.innerHTML = '';
   
@@ -31,11 +50,21 @@ handlePortfolio.addEventListener("click", () => {
 })
 
 handleServices.addEventListener("click",()=>{
+    handleHome.style.color = '#B2C8BA'
+    handleAbout.style.color = "#B2C8BA";
+    handlePortfolio.style.color = "#B2C8BA";
+    handleServices.style.color = "#ACD793";
+    handleContact.style.color = "#B2C8BA";
     headingValue.innerHTML = "Services";
     paraValue.innerHTML = paraServices;
     boxValue.innerHTML = oldBoxValue;
 })
 handleContact.addEventListener("click",()=>{
+    handleHome.style.color = '#B2C8BA'
+    handleAbout.style.color = "#B2C8BA";
+    handlePortfolio.style.color = "#B2C8BA";
+    handleServices.style.color = "#B2C8BA";
+    handleContact.style.color = "#ACD793";
     headingValue.innerHTML = "Contact";
     paraValue.innerHTML = contactContent;
     boxValue.innerHTML = '';
